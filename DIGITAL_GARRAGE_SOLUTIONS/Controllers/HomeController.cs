8877ccd134e8d47666b2d9d3522bb7200b9439b0
HP <HP@DESKTOP-VMBGFKO>
2025-12-03 15:07:57 +0530

    Add project files.

---
+++ DIGITAL_GARRAGE_SOLUTIONS/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using DIGITAL_GARRAGE_SOLUTIONS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DIGITAL_GARRAGE_SOLUTIONS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DIGITAL_GARRAGE_SOLUTIONS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DIGITAL_GARRAGE_SOLUTIONS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
